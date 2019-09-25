169eb793dc2c98957d98ee98dcbcae6af29e07a4
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-25 10:41:49 +0100

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,23 @@
+class StaticController < ApplicationController
+  def home
+    render 'home'
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
